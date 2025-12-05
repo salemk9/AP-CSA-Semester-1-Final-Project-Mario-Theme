cb4892b29c3f19e5ff2d1aa1d1e3321e50282c32
Peri-0z <peridraws020@gmail.com>
2025-12-05 10:18:17 -0800

    Add JavaDoc comments to Wario class alongside setters and getters.
    Added JavaDoc comments for Wario class methods and constructor.

--- Wario.java
+++ Wario.java
@@ -1,3 +1,6 @@
+/**
+ * Represents the character Wario with various attributes such as health, wealth, musk, size, and mood.
+ */
 public class Wario {
     
     private int health;
@@ -5,8 +8,17 @@ public class Wario {
     private int musk;
     private String size;
     private String mood;
-    
-    public Wario(int hp, int wealth, int stink, String size, String mood){
+
+    /**
+     * Constructs a Wario object with the specified attributes.
+     * 
+     * @param hp    the health of Wario
+     * @param wealth the wealth of Wario
+     * @param stink the musk level of Wario
+     * @param size  the size of Wario
+     * @param mood  the mood of Wario
+     */
+    public Wario(int hp, int wealth, int stink, String size, String mood) {
         this.health = hp;
         this.wealth = wealth;
         this.musk = stink;
@@ -14,7 +26,10 @@ public class Wario {
         this.mood = mood;
     }
 
-    public void displayStatus(){
+    /**
+     * Displays the current status of Wario.
+     */
+    public void displayStatus() {
         System.out.println("Wario's Health: " + health);
         System.out.println("Wario's Wealth: " + wealth);
         System.out.println("Wario's Stinkiness: " + musk);
@@ -22,4 +37,93 @@ public class Wario {
         System.out.println("Wario's Mood: " + mood);
     }
 
+    /**
+     * Gets the health of Wario.
+     * 
+     * @return the health of Wario
+     */
+    public int getHealth() {
+        return health;
+    }
+
+    /**
+     * Sets the health of Wario.
+     * 
+     * @param health the new health value
+     */
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    /**
+     * Gets the wealth of Wario.
+     * 
+     * @return the wealth of Wario
+     */
+    public int getWealth() {
+        return wealth;
+    }
+
+    /**
+     * Sets the wealth of Wario.
+     * 
+     * @param wealth the new wealth value
+     */
+    public void setWealth(int wealth) {
+        this.wealth = wealth;
+    }
+
+    /**
+     * Gets the musk level of Wario.
+     * 
+     * @return the musk level of Wario
+     */
+    public int getMusk() {
+        return musk;
+    }
+
+    /**
+     * Sets the musk level of Wario.
+     * 
+     * @param musk the new musk level
+     */
+    public void setMusk(int musk) {
+        this.musk = musk;
+    }
+
+    /**
+     * Gets the size of Wario.
+     * 
+     * @return the size of Wario
+     */
+    public String getSize() {
+        return size;
+    }
+
+    /**
+     * Sets the size of Wario.
+     * 
+     * @param size the new size value
+     */
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    /**
+     * Gets the mood of Wario.
+     * 
+     * @return the mood of Wario
+     */
+    public String getMood() {
+        return mood;
+    }
+
+    /**
+     * Sets the mood of Wario.
+     * 
+     * @param mood the new mood value
+     */
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
 }
