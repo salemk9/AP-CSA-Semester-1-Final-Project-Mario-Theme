4aa988d0a52aa9f97a043a2995c7bd5f1db92e86
Peri-0z <peridraws020@gmail.com>
2025-12-10 10:05:17 -0800

    Update Wario's health attribute in TesterMain

--- TesterMain.java
+++ TesterMain.java
@@ -1,6 +1,13 @@
 public class TesterMain {
     public static void main(String[] args) {
-        Wario wario = new Wario(100, 1000, 10, "Large", "Greedy");
+        Wario wario = new Wario(100, 1500, 10, "Large", "Greedy");
+        wario.displayStatus();
+
+        System.out.println("--- Wario takes action ---");
+        wario.mugSomeone();
+        wario.eatGarlic();
+        wario.donateCharity();
+        wario.releaseSteam();
         wario.displayStatus();
     }
 }
